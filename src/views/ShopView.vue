13a76d7fa95c4b52e40ac45431b939d8722e12f8
Karen Rodriguez <kyrodriguez@gmail.com>
2023-09-15 15:17:55 -0500

    Ajuestes navegacion

---
+++ src/views/ShopView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainNav  from "../components/MainNav.vue";
+</script>
+
+<template>
+  <div>
+    <MainNav />
+    <h1>Shop View</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
