131cc8050a1ed6f4b0dd412dc3a8da28834e25df
Karen Rodriguez <kyrodriguez@gmail.com>
2023-10-16 12:00:37 -0500

    Mostrando productos

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useFirestore, useCollection, useFirebaseStorage } from 'vuefire';
 import { collection, addDoc, where, query, limit, orderBy, updateDoc, doc, getDoc, deleteDoc } from 'firebase/firestore';
 import { ref as storageRef, deleteObject } from 'firebase/storage';
@@ -9,6 +9,7 @@ export const useProductsStore = defineStore('products', () => {
     const db = useFirestore();
     const storage = useFirebaseStorage();
 
+    const selectCategory=ref(1);
     const categories = [
         { id: 1, name: 'Sudabderas' },
         { id: 2, name: 'Tenis' },
@@ -64,12 +65,19 @@ export const useProductsStore = defineStore('products', () => {
 
     const noResults = computed(() => productsCollection.value.length === 0)
 
+    const filteredProducts = computed(() => {
+        return productsCollection.value.filter(product => product.category === selectCategory.value)
+    })
+
     return {
         createProduct,
         updateProduct,
         deleteProduct,
         productsCollection,
+        categories,
+        selectCategory,
         categoryOptions,
-        noResults
+        noResults,
+        filteredProducts
     }
 })
